c9cd14bf8d523e628c75b79688c50ad00bca9177
RagyGobrail <eskander.ragy@gmail.com>
2026-05-05 16:08:35 +0300

    added Vercal req

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,7 @@ app = FastAPI(title="Spotify Recommender")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Frontend origin
+    allow_origins=["*"],  # Frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
